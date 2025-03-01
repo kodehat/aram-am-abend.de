8ce64fa3f4fe0df9bb049531583a3e4fedbdcf24
kodehat <dev@codehat.de>
2025-03-01 14:58:40 +0100

    Remove unused code

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,4 @@ import rules from './rules.js'
 window.Alpine = Alpine;
 
 Alpine.data('rules', rules);
-Alpine.data('mode', () => ({
-    mode: null,
-    init() {
-        if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
-            this.mode = 'development';
-            localStorage.setItem('umami.disabled', 1);
-            console.log('Development mode');
-        } else {
-            this.mode = 'production';
-        }
-    }
-}))
 Alpine.start();
