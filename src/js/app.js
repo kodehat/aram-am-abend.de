4f680fcbf6ccbeb32cd862aa58758417c2715799
kodehat <dev@codehat.de>
2025-03-01 11:51:03 +0100

    Update dependencies and analytics

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,16 @@ import rules from './rules.js'
 window.Alpine = Alpine;
 
 Alpine.data('rules', rules);
+Alpine.data('mode', () => ({
+    mode: null,
+    init() {
+        if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
+            this.mode = 'development';
+            localStorage.setItem('umami.disabled', 1);
+            console.log('Development mode');
+        } else {
+            this.mode = 'production';
+        }
+    }
+}))
 Alpine.start();
